3cfb86590ecc987a9bfbe813cbe7d6a9620924a0
alijohnsoncodes <110363518+alijohnsoncodes@users.noreply.github.com>
2023-02-20 21:52:54 +1100

    Added date and time JS function

--- src/app.js
+++ src/app.js
@@ -1,17 +1,40 @@
+function formatDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes =`0${minutes}`;
+}
+
+let days =["Sunday",
+"Monday", 
+"Tuesday", 
+"Wednesday", 
+"Thursday", 
+"Friday", 
+"Saturday"];
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
     console.log(response.data);//temperature
-    let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
-   // let precipitationElement = document.querySelector("#precipitation");
+    let dateElement = document.querySelector("#date");
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
     windElement.innerHTML = Math.round(response.data.wind.speed);
-   // precipitationElement.innerHTML = Math.round(response.data.)
+   dateElement.innerHTML = formatDate(response.data.time*1000);
 }
 
 
@@ -19,6 +42,6 @@ let apiKey = "ff67f897a48b47at483eo6903fc267cb";
 let apiUrl =`https://api.shecodes.io/weather/v1/current?
 query=New York&key=${apiKey}&units=metric`;
 
-console.log(apiUrl);
+//console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
