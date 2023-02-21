66ab49e35665d6255995f122648d93333db3a3ed
alijohnsoncodes <110363518+alijohnsoncodes@users.noreply.github.com>
2023-02-21 21:08:15 +1100

    Forecast for next 5 days introduced

--- src/app.js
+++ src/app.js
@@ -22,29 +22,40 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
+    console.log(response);
 
     let forecastHTML =`<div class="row">`;
-    let days =["Tues", "Wed", "Thurs", "Fri", "Sat"];
-    days.forEach(function(day) {
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 5) {
+        console.log
     forecastHTML = forecastHTML +
     `
     <div class="col-2">
      <div class="weather-forecast-date">
-       ${day}
+       ${formatDay(forecastDay.time)}
      </div>
-     <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+     <img src="https://ssl.gstatic.com/onebox/weather/64/${forecastDay.condition.icon}.png" 
      alt="" 
      width="42"
      />
      <div class="weather-forecast-temperatures">
-     <span class="weather-forecast-temperature-max"> 18° </span>
-     <span class="weather-forecast-temperature-min"> 12° </span>
+     <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temperature.maximum)}° </span>
+     <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temperature.minimum)}° </span>
    </div>
    </div>
  `;
+        }
 });
  forecastHTML = forecastHTML + `</div>`;
  forecastElement.innerHTML = forecastHTML;
